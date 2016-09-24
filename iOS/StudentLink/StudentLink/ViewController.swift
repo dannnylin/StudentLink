2b0daa116832780cf33e44866c84a7a1000a3be6
Danny Tan <dannytan.ce@gmail.com>
2016-09-24 17:28:06 -0400

    New Project

---
+++ iOS/StudentLink/StudentLink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudentLink
+//
+//  Created by Danny Tan on 9/24/16.
+//  Copyright © 2016 StudentLink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
