eeb3f6bd3e6d7edc713a25af101810896b31bb37
Danny Lin <danny.lin@nyu.edu>
2016-09-24 19:54:06 -0400

    handle fb login

--- iOS/StudentLink/StudentLink/Controllers/LoginViewController.swift
+++ iOS/StudentLink/StudentLink/Controllers/LoginViewController.swift
@@ -8,15 +8,56 @@
 
 import UIKit
 import FBSDKLoginKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = FBSDKLoginButton()
-        loginButton.center = self.view.center
-        self.view.addSubview(loginButton)
-   
+    }
+    
+    @IBAction func loginPressed(sender: UIButton) {
+        loginButtonClicked()
+    }
+    
+    func loginButtonClicked() {
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logInWithReadPermissions(["email"], fromViewController: self) { (result, error) -> Void in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if result.isCancelled {
+                print("Facebook login cancelled")
+            } else {
+                let fbloginresult : FBSDKLoginManagerLoginResult = result
+                if(fbloginresult.grantedPermissions.contains("email"))
+                {
+                    self.getFBUserData()
+                    
+                    let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
+                    FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
+                        // Login successful
+                        if let error = error {
+                            print(error)
+                        }
+                        
+                        // move to home
+                        self.view.window?.rootViewController = MainTabBarController.create()
+                    }
+                }
+            }
+        }
+    }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.currentAccessToken()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print(result["email"])
+                }
+            })
+        }
     }
 
     class func create() -> LoginViewController {
